61bddcce28ba6b7ef9724989b9a56731b6aee1dd
MD <manjubd437@gmail.com>
2026-08-28 09:30:02 +0530

    Enhance deleteArticle function with alerts

--- admin.js
+++ admin.js
@@ -1,8 +1,9 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwTvaf6H0ETygmY5bRJXP_5QgLYaeIYcXAjp0JtCaMB1mCxa_TMYKPaIZ5hTUCyGoqAVg/exec";
 
-alert("admin.js loaded");
-
 async function deleteArticle(id){
+
+  alert("Delete clicked: " + id);
+
 
   if(!confirm("ಈ ಲೇಖನವನ್ನು delete ಮಾಡಬೇಕೆ?")){
     return;
@@ -17,23 +18,20 @@ async function deleteArticle(id){
 
 
   const response =
-  await fetch(API_URL,{
-    method:"POST",
-    headers:{
-      "Content-Type":
-      "application/x-www-form-urlencoded;charset=UTF-8"
-    },
-    body:form.toString()
-  });
+    await fetch(API_URL,{
+      method:"POST",
+      headers:{
+        "Content-Type":
+        "application/x-www-form-urlencoded;charset=UTF-8"
+      },
+      body:form.toString()
     });
 
 
-  const text =
+  const result =
     await response.text();
 
 
-  alert(text);
-
-  console.log(text);
+  alert(result);
 
 }
